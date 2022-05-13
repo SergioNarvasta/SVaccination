8dc8db3edbb0ce13c57d1f6a1ed3f13345a2f31b
Sergio Narvasta <sergio.jn024@gmail.com>
2022-05-13 12:20:00 -0500

    Start

---
+++ registro.php
@@ -0,0 +1,94 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel=StyleSheet href="estilos.css" type="text/css">
+        <title>Registro de Pacientes</title>
+    </head>
+    <body>
+        <br><br>
+    <h1 align="center">FORMATO NOMINAL</h1>
+    <br>
+    <form class="frmRegistro" action="p_registro.php" method="post">
+        <fieldset >
+        <legend>Datos del paciente</legend>
+        <table align="center">
+            <tr>
+                <td>DNI</td>
+                <td><input type="text" name="txtdni" required="true" ></td></td>
+            </tr>
+            <tr>
+                <td>AP PATERNO</td>
+                <td>AP MATERNO</td>
+                <td>NOMBRES</td>
+            </tr>
+            <tr>
+                <td><input type="text" name="txtpaterno" autocomplete="off" required="true" ></td>
+                <td><input type="text" name="txtmaterno" autocomplete="off" required="true" ></td>
+                <td><input type="text" name="txtnombres" autocomplete="off" required="true" ></td>
+            </tr>
+            <tr>
+                <td>GENERO</td>
+                <td>EDAD</td>
+                <td>FECHA DE NACIMIENTO</td>
+            </tr>
+            <tr>
+                <td><input type="radio" name="rbtgenero" value="M">Masculino
+                    <input type="radio" name="rbtgenero" value="F">Femenino</td>
+                <td><input type="text" name="txtedad"></td>
+                <td><input type="date" name="fechanac"></td>
+            </tr>
+            <tr>
+                <td>CELULAR</td>
+                <td colspan="2">DIRECCION</td>  
+            </tr>
+            <tr>
+                <td><input type="text" name="txtcelular"></td>
+                <td><input type="text" name="txtdireccion"></td>
+            </tr>
+        </table>
+       </fieldset>
+    <br>
+    <fieldset>
+        <legend>Datos del paciente</legend>
+        <table align="center">
+            <tr>
+                <td>Dosis</td>
+                <td>Fecha de Dosis</td>
+                <td>Centro de Vacunacion</td>
+            </tr>
+            <tr>
+                <td><select name="txtdosis">
+                    <option value="1era Dosis">1era Dosis</option>
+                    <option value="2da Dosis">2da Dosis</option>
+                    <option value="3era Dosis">3era Dosis</option>
+                    <option value="4ta Dosis">4ta Dosis</option>
+                </select></td>
+                <td><input type="date" name="fechadosis"></td>
+                <td><select name="lstcentrov">
+                    <option value="Hospital Regional Huacho">Hospital Regional Huacho</option>
+                    <option value="Posta de Salud Huaura">Posta de Salud Huaura</option>
+                    <option value="Posta de Salud Manzanares">Posta de Salud Manzanares</option>
+                </select> </td>
+            </tr>
+            <tr>
+                <td colspan="6">Factor de Riesgo</td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="lstfactores" value="Normal">Normal</td>
+                <td><input type="checkbox" name="lstfactores" value="Sindrome de Down">Sindrome de Down</td>
+                <td><input type="checkbox" name="lstfactores" value="Dialisis">Dialisis</td>
+                <td><input type="checkbox" name="lstfactores" value="60-69 años">60-69 años</td>
+                <td><input type="checkbox" name="lstfactores" value="70-79 años">70-79 años</td>
+                <td><input type="checkbox" name="lstfactores" value="80 a mas">80 a mas</td>
+            </tr>
+            
+        </table>
+    </fieldset>
+    <br>
+            <input class="btnregistrar" type="submit" value="Registrar">              
+ 
+    </form>
+
+    </body>
+
+</html>
---
+++ reporte.php
@@ -0,0 +1,114 @@
+<?php 
+
+$fila=file("registro.xls");
+
+for($i=0; $i<count($fila); $i++){
+
+list($dni,$pat,$mat,$nom,$gen,$ed,$fen,$cel,$dir,$dos,$fed,$cenv,$fac) =explode("\t",$fila[$i]);
+}
+$paciente=$pat." ".$mat." ".$nom;
+$hombres=3;
+$mujeres=0;
+$mayores=1;
+$menores=0;
+$riesgo=0;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">   
+    <title>REPORTE</title>
+    <link rel=StyleSheet href="estilos.css" type="text/css">
+</head>
+<body class="reporte">
+    <br><br>
+    <h2 align="center" >PADRON OFICIAL</h2>
+    <br>
+    <table class="tablerep">
+     <tr>
+         <td>DNI</td>
+         <td>NOMBRES</td>
+         <td>GENERO</td>
+         <td>EDAD</td>
+         <td>FECHA NAC</td>
+         <td>CELULAR</td>
+         <td>DIRECCION</td>
+         <td>DOSIS</td>
+         <td>FECHA DOSIS</td>
+         <td>CENTRO VAC</td>
+         <td>FACTORES</td>
+     </tr>
+     <tr>
+         <td><?php echo $dni?></td>
+         <td><?php echo $paciente?></td>
+         <td><?php echo $gen?></td>
+         <td><?php echo $ed?></td>
+         <td><?php echo $fen?></td>
+         <td><?php echo $cel?></td>
+         <td><?php echo $dir?></td>
+         <td><?php echo $dos?></td>
+         <td><?php echo $fed?></td>
+         <td><?php echo $cenv?></td>
+         <td><?php echo $fac?></td>
+        
+     </tr>
+     <tr>
+     <td><?php echo $dni?></td>
+         <td><?php echo $paciente?></td>
+         <td><?php echo $gen?></td>
+         <td><?php echo $ed?></td>
+         <td><?php echo $fen?></td>
+         <td><?php echo $cel?></td>
+         <td><?php echo $dir?></td>
+         <td><?php echo $dos?></td>
+         <td><?php echo $fed?></td>
+         <td><?php echo $cenv?></td>
+         <td><?php echo $fac?></td>
+     </tr>
+     <tr>
+     <td><?php echo $dni?></td>
+         <td><?php echo $paciente?></td>
+         <td><?php echo $gen?></td>
+         <td><?php echo $ed?></td>
+         <td><?php echo $fen?></td>
+         <td><?php echo $cel?></td>
+         <td><?php echo $dir?></td>
+         <td><?php echo $dos?></td>
+         <td><?php echo $fed?></td>
+         <td><?php echo $cenv?></td>
+         <td><?php echo $fac?></td>
+     </tr>
+     <tr>
+
+
+    </table>
+    <br><br>
+    <fieldset class="estadisticas">
+        <legend>ESTADISTICAS</legend>
+   <table align="center" >
+       <tr>
+           <td>Hombres</td>
+           <td><?php echo $hombres?></td>
+       </tr>
+       <tr>
+           <td>Mujeres</td>
+           <td><?php echo $mujeres?></td>
+       </tr>
+       <tr>
+           <td>Mayores</td>
+           <td><?php echo $mayores?></td>
+       </tr>
+       <tr>
+           <td>Menores</td>
+           <td><?php echo $menores?></td>
+       </tr>
+       <tr>
+           <td>F de Riesgo</td>
+           <td><?php echo $riesgo?></td>
+       </tr>
+   </table >
+   </fieldset>
+</body>
+</html>
