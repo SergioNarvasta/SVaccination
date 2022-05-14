c92f962b5ee5c268235493883c54607aef1cc29a
Sergio Narvasta <sergio.jn024@gmail.com>
2022-05-14 01:10:55 -0500

    dev

--- consulta.php
+++ consulta.php
@@ -4,12 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consulta</title>
 </head>
 <body>
-<center><h1>Operaciones</h1></center>
-<form action="procesooperaciones.php" method="post">
+<br><br>
+<center><h1 class="textCarnet">Carnet de </h1></center>
+<center><h1 class="textVacunacion">Vacunacion </h1></center>
+<center><h3 class="textVacunacion">Ingrese los Datos Solicitados</h3></center><br>
 
+<form action=".php" method="post">
+<table class="tblconsulta" align="center">
+    <tr>
+        <td>Numero de Documento</td>
+    </tr>
+    <tr>
+        <td><input  type="text" value=""> </td>
+    </tr>
+    <tr>
+        <td>Fecha de Nacimiento(dd/mm/yyyy)</td>
+    </tr>
+    <tr>
+        <td><input  type="text" value=""></td>
+    </tr>
+    <tr> 
+        <td><input class="btnConsulta" type="submit" value=">> INICIAR SESION"></td>
+    </tr>
+</table>
 
 </form>
 
