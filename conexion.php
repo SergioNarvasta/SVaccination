31c0e8258e467ef18aa3126bf1b63f62343c3878
Sergio Narvasta <sergio.jn024@gmail.com>
2022-05-16 23:13:51 -0500

    dev

--- conexion.php
+++ conexion.php
@@ -1,14 +1,13 @@
 <?php
+
+
 $server = "DESKTOP-2NA2N4M\SQLEXPRESS";
 $database = "BDSystemV";
 $username = "Sergio";
 $password = "1234";  
 
-
-
   class Conexion{
     function conexionBD(){   
-
         
         try{
         $conexion = new PDO("sqlsrv:server=$server;database=$database,$username,$password");
